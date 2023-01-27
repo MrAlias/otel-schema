ff052d0f55a8fce786ad9e23001b404e0364952d
Alex Boten <aboten@lightstep.com>
2023-01-27 08:39:50 -0800

    document example better
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- json_schema/python/validate.py
+++ json_schema/python/validate.py
@@ -3,6 +3,7 @@
 import yaml
 from pathlib import Path
 from jsonschema import validate, validators
+from jsonschema.exceptions import ValidationError
 
 path = Path(__file__).parent.resolve()
 resolver = validators.RefResolver(
@@ -10,14 +11,21 @@ resolver = validators.RefResolver(
     referrer=True,
 )
 
-with open("../kitchen-sink.yaml", "r") as stream:
+config_file = "../kitchen-sink.yaml"
+print(f"Using config file: {config_file}")
+with open(config_file, "r") as stream:
     try:
         parse_config = yaml.safe_load(stream)
+        print("YAML parsed successfully")
+        schema_file = "../schema/schema.json"
+        print(f"Validating using schema file: {schema_file}")
         result = validate(
             instance=parse_config,
-            schema={"$ref": "../schema/schema.json"},
+            schema={"$ref": schema_file},
             resolver=resolver,
         )
-        print(result)
+        print("No validation errors")
     except yaml.YAMLError as exc:
+        print(exc)
+    except ValidationError as exc:
         print(exc)
