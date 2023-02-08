22630a78d5304c2cf283a9fb4a9204fe8cfb6891
Jack Berg <jberg@newrelic.com>
2023-02-08 15:33:44 -0600

    Update java json schema validation

--- json_schema/java/src/test/java/io/opentelemetry/fileconfig/SdkSchemaTest.java
+++ json_schema/java/src/test/java/io/opentelemetry/fileconfig/SdkSchemaTest.java
@@ -3,24 +3,21 @@ package io.opentelemetry.fileconfig;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import org.junit.jupiter.api.Test;
 
 class SdkSchemaTest {
 
-  private static final SchemaValidationExtension validationExtension =
-      new SchemaValidationExtension("sdk.json");
-
   @Test
-  void kitchenSink() {
+  void kitchenSink() throws FileNotFoundException {
+    YamlJsonSchemaValidator validator =
+        new YamlJsonSchemaValidator(new File(System.getenv("SCHEMA_FILE")));
+
+    FileInputStream fis =
+        new FileInputStream(System.getenv("REPO_DIR") + "/json_schema/kitchen-sink.yaml");
+
     // Validate example kitchen-sink file in base of repository
-    assertThat(
-            validationExtension.validateFile(
-                new File(System.getProperty("REPO_DIR") + "/json_schema/kitchen-sink.yaml")))
-        .isEmpty();
-  }
-
-  @Test
-  void simple() {
-    assertThat(validationExtension.validateResource("/sdk/simple.yaml")).isEmpty();
+    assertThat(validator.validate(fis)).isEmpty();
   }
 }
